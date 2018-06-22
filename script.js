f2b62af7a9412801ca2b66b35b76769b645e541a
kevin <kevinelsen@hotmail.fr>
2018-06-22 09:11:33 +0200

    exo7

---
+++ script.js
@@ -0,0 +1,10 @@
+function calcul() {
+  var shoeSize = document.getElementById('shoeSize').value;
+  var yearOfBirth = document.getElementById('yearOfBirth').value;
+  var result = shoeSize * 2;
+  result = result + 5;
+  result = result * 50;
+  result = result - yearOfBirth;
+  result = result + 1766;
+  alert(shoeSize yearOfBirth);
+}
